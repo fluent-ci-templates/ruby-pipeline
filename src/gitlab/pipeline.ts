4b380ddaf4bde67c7c2f8ebd8646ba5a2c353486
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-07-05 13:50:04 +0000

    update default pipeline jobs

--- src/gitlab/pipeline.ts
+++ src/gitlab/pipeline.ts
@@ -1,10 +1,10 @@
 import { GitlabCI } from "https://deno.land/x/fluent_gitlab_ci@v0.3.2/mod.ts";
-import { rubocop } from "./jobs.ts";
+import { deploy, rails, rspec, rubocop } from "./jobs.ts";
 
 const gitlabci = new GitlabCI()
   .addJob("rubocop", rubocop)
-  .addJob("rails", rubocop)
-  .addJob("rspec", rubocop)
-  .addJob("deploy", rubocop);
+  .addJob("rails", rails)
+  .addJob("rspec", rspec)
+  .addJob("deploy", deploy);
 
 export default gitlabci;
