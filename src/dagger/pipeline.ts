c0abf335b361bb49b92ee61c84970c523d7a1336
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-04 19:07:25 +0000

    feat: allow running specific jobs from a pipeline

--- src/dagger/pipeline.ts
+++ src/dagger/pipeline.ts
@@ -1,10 +1,28 @@
 import Client, { connect } from "@dagger.io/dagger";
-import { rails, rspec, rubocop } from "./jobs.ts";
+import * as jobs from "./jobs.ts";
 
-export default function pipeline(src = ".") {
+const { rubocop, rails, rspec } = jobs;
+
+export default function pipeline(src = ".", args: string[] = []) {
   connect(async (client: Client) => {
+    if (args.length > 0) {
+      await runSpecificJobs(client, args);
+      return;
+    }
+
     await rubocop(client, src);
     await rails(client, src);
     await rspec(client, src);
   });
+}
+
+async function runSpecificJobs(client: Client, args: string[]) {
+  for (const name of args) {
+    // deno-lint-ignore no-explicit-any
+    const job = (jobs as any)[name];
+    if (!job) {
+      throw new Error(`Job ${name} not found`);
+    }
+    await job(client);
+  }
 }
