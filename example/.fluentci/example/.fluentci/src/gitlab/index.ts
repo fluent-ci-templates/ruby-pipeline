494bae4f41238947256b61d3cf12926cf9d013fe
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-18 06:55:37 +0000

    make it compatible with Dagger Zenith

--- example/.fluentci/example/.fluentci/src/gitlab/index.ts
+++
@@ -1,4 +0,0 @@
-import pipeline from "./pipeline.ts";
-import { rubocop, rails, rspec, herokuDeploy } from "./jobs.ts";
-
-export { pipeline, rubocop, rails, rspec, herokuDeploy };
--- example/.fluentci/src/dagger/index.ts
+++
@@ -1,4 +0,0 @@
-import pipeline from "./pipeline.ts";
-import { rubocop, rails, rspec } from "./jobs.ts";
-
-export { pipeline, rubocop, rails, rspec };
