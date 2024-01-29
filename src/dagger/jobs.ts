59f0e893b9f57adcbe3088229cc83300260e71ce
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-29 06:35:14 +0000

    fix issue with dagger connect

--- src/dagger/jobs.ts
+++ src/dagger/jobs.ts
@@ -1,5 +1,4 @@
-import { Client, Directory } from "../../sdk/client.gen.ts";
-import { connect } from "../../sdk/connect.ts";
+import { dag, Directory } from "../../sdk/client.gen.ts";
 import { getDirectory } from "./lib.ts";
 
 export enum Job {
@@ -19,34 +18,30 @@ export const exclude = ["vendor", ".git", ".devbox", ".fluentci"];
 export async function rubocop(
   src: Directory | string | undefined = "."
 ): Promise<string> {
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const baseCtr = client
-      .pipeline(Job.rubocop)
-      .container()
-      .from("ghcr.io/fluentci-io/devbox:latest")
-      .withExec(["sh", "-c", "which nix"])
-      .withExec(["sh", "-c", "nix --version"]);
-    const ctr = baseCtr
-      .withMountedCache("/app/vendor", client.cacheVolume("bundle-cache"))
-      .withDirectory("/app", context, {
-        exclude,
-      })
-      .withWorkdir("/app")
-      .withExec(["sh", "-c", "devbox run -- ruby --version"])
-      .withExec([
-        "sh",
-        "-c",
-        "devbox run -- bundle config set --local deployment true",
-      ])
-      .withExec(["sh", "-c", "devbox run -- bundle install -j $(nproc)"])
-      .withExec(["sh", "-c", "devbox run -- bundle exec rubocop"]);
+  const context = await getDirectory(dag, src);
+  const baseCtr = dag
+    .pipeline(Job.rubocop)
+    .container()
+    .from("ghcr.io/fluentci-io/devbox:latest")
+    .withExec(["sh", "-c", "which nix"])
+    .withExec(["sh", "-c", "nix --version"]);
+  const ctr = baseCtr
+    .withMountedCache("/app/vendor", dag.cacheVolume("bundle-cache"))
+    .withDirectory("/app", context, {
+      exclude,
+    })
+    .withWorkdir("/app")
+    .withExec(["sh", "-c", "devbox run -- ruby --version"])
+    .withExec([
+      "sh",
+      "-c",
+      "devbox run -- bundle config set --local deployment true",
+    ])
+    .withExec(["sh", "-c", "devbox run -- bundle install -j $(nproc)"])
+    .withExec(["sh", "-c", "devbox run -- bundle exec rubocop"]);
 
-    const result = await ctr.stdout();
-
-    console.log(result);
-  });
-  return "Done";
+  const result = await ctr.stdout();
+  return result;
 }
 
 /**
@@ -58,35 +53,31 @@ export async function rubocop(
 export async function rails(
   src: Directory | string | undefined = "."
 ): Promise<string> {
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const baseCtr = client
-      .pipeline(Job.rails)
-      .container()
-      .from("ghcr.io/fluent-ci-templates/devbox:latest");
+  const context = await getDirectory(dag, src);
+  const baseCtr = dag
+    .pipeline(Job.rails)
+    .container()
+    .from("ghcr.io/fluent-ci-templates/devbox:latest");
 
-    const ctr = baseCtr
-      .withMountedCache("/app/vendor", client.cacheVolume("bundle-cache"))
-      .withDirectory("/app", context, { exclude })
-      .withWorkdir("/app")
-      .withExec(["sh", "-c", "devbox run -- ruby --version"])
-      .withExec([
-        "sh",
-        "-c",
-        "devbox run -- bundle config set --local deployment true",
-      ])
-      .withExec(["sh", "-c", "which nix"])
-      .withExec(["sh", "-c", "nix --version"])
-      .withExec(["sh", "-c", "devbox run -- bundle install -j $(nproc)"])
-      .withExec(["sh", "-c", "devbox run -- bundle exec rails db:migrate"])
-      .withExec(["sh", "-c", "devbox run -- bundle exec rails db:seed"])
-      .withExec(["sh", "-c", "devbox run -- bundle exec rails test"]);
+  const ctr = baseCtr
+    .withMountedCache("/app/vendor", dag.cacheVolume("bundle-cache"))
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withExec(["sh", "-c", "devbox run -- ruby --version"])
+    .withExec([
+      "sh",
+      "-c",
+      "devbox run -- bundle config set --local deployment true",
+    ])
+    .withExec(["sh", "-c", "which nix"])
+    .withExec(["sh", "-c", "nix --version"])
+    .withExec(["sh", "-c", "devbox run -- bundle install -j $(nproc)"])
+    .withExec(["sh", "-c", "devbox run -- bundle exec rails db:migrate"])
+    .withExec(["sh", "-c", "devbox run -- bundle exec rails db:seed"])
+    .withExec(["sh", "-c", "devbox run -- bundle exec rails test"]);
 
-    const result = await ctr.stdout();
-
-    console.log(result);
-  });
-  return "Done";
+  const result = await ctr.stdout();
+  return result;
 }
 
 /**
@@ -98,32 +89,28 @@ export async function rails(
 export async function rspec(
   src: Directory | string | undefined = "."
 ): Promise<string> {
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const baseCtr = client
-      .pipeline(Job.rspec)
-      .container()
-      .from("ghcr.io/fluent-ci-templates/devbox:latest");
+  const context = await getDirectory(dag, src);
+  const baseCtr = dag
+    .pipeline(Job.rspec)
+    .container()
+    .from("ghcr.io/fluent-ci-templates/devbox:latest");
 
-    const ctr = baseCtr
-      .withMountedCache("/app/vendor", client.cacheVolume("bundle-cache"))
-      .withDirectory("/app", context, { exclude })
-      .withWorkdir("/app")
-      .withExec(["sh", "-c", "devbox run -- ruby --version"])
-      .withExec([
-        "sh",
-        "-c",
-        "devbox run -- bundle config set --local deployment true",
-      ])
-      .withExec(["sh", "-c", "devbox run -- gem install rspec"])
-      .withExec(["sh", "-c", "devbox run -- bundle install -j $(nproc)"])
-      .withExec(["sh", "-c", "devbox run -- rspec spec"]);
+  const ctr = baseCtr
+    .withMountedCache("/app/vendor", dag.cacheVolume("bundle-cache"))
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withExec(["sh", "-c", "devbox run -- ruby --version"])
+    .withExec([
+      "sh",
+      "-c",
+      "devbox run -- bundle config set --local deployment true",
+    ])
+    .withExec(["sh", "-c", "devbox run -- gem install rspec"])
+    .withExec(["sh", "-c", "devbox run -- bundle install -j $(nproc)"])
+    .withExec(["sh", "-c", "devbox run -- rspec spec"]);
 
-    const result = await ctr.stdout();
-
-    console.log(result);
-  });
-  return "Done";
+  const result = await ctr.stdout();
+  return result;
 }
 
 export type JobExec = (src?: Directory | string) => Promise<string>;
